195d0ac734fb8c73d60da8f6cd05d31d409ed371
EsselKobby <essel.bernard.kobby@gmail.com>
2024-12-20 15:44:03 +0000

    appointment model

--- base/models.py
+++ base/models.py
@@ -15,3 +15,24 @@ class Service(model.Models):
 
     def __str__(self):
         return f"{self.name} - {self.cost}" 
+
+class Appointment(models.Model):
+    STATUS = [
+        ('Scheduled', 'Scheduled'),
+        ('Completed', 'Completed'),
+        ('Pending', 'Pending'),
+        ('Cancelled', 'Cancelled'),
+
+    ]
+
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True, blank=True, related_name='service_appointed')
+    doctor = models.ForeignKey(doctor_models.Doctor, on_delete=models.SET_NULL, null=True, blank=True, related_name='doctor_appointments')
+    patient = models.ForeignKey(patient_models.Patient, on_delete=models.SET_NULL, null=True, blank=True, related_name='appointments_patient')
+    appointment_date = models.DateTimeField(null=True, blank=True)
+    issues = models.TextField(blank=True, null=True)
+    symptoms = models.TextField(blank=True, null=True)
+    appointment_id = ShortUUIDField(length=6, max_length=10, alphabet="1234567890")
+    status = models.CharField(max_length=120, choices=STATUS)
+
+    def __str__(self):
+        return f"{self.patient.full_name} with {self.doctor.full_name}"
