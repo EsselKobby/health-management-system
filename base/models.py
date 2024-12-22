cf72826d8fe3bf37143ded0c652d3aa33ac2b5e2
EsselKobby <essel.bernard.kobby@gmail.com>
2024-12-22 00:44:33 +0000

    doctors correction to doctor

--- base/models.py
+++ base/models.py
@@ -11,7 +11,7 @@ class Service(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
-    available_doctors = models.ManyToManyField(doctors_models.Doctor, blank=True)
+    available_doctors = models.ManyToManyField(doctor_models.Doctor, blank=True)
 
     def __str__(self):
         return f"{self.name} - {self.cost}" 
